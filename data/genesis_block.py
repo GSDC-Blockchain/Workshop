cecfe3353aa62cbef9d6284dc1050427422f61b5
Filip <filipdj98@gmail.com>
2020-11-11 11:09:06 +0100

    Main branch

--- data/genesis_block.py
+++ data/genesis_block.py
@@ -6,5 +6,4 @@ GENESIS_HASH = "000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f
 class GenesisBlock (Block) :
     def __init__(self):
         super().__init__("")
-        self.info = "THE GENESIS BLOCK"
-    
+        self.info = "THE GENESIS BLOCK "
